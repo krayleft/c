63e4a3ed6424022f285ab4090e8a7841012d7f46
krayleft <70761416+krayleft@users.noreply.github.com>
2023-09-19 14:11:22 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     float celsius, fahrenheit;
+
+//     cout << "Введите температуру по Цельсию: ";
+//     cin >> celsius;
+
+//     fahrenheit = (celsius * 9) / 5 + 32;
+
+//     cout << "Температура по Фаренгейту: " << fahrenheit << " F" << endl;
+
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    a++;
+    a -= 3;
+    a %= 2;
+    cout << "Результат: " << a << endl;
+
+    return 0;
+}
