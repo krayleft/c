f38a2cbc3586f43285f4e15559967ab5443a2056
krayleft <70761416+krayleft@users.noreply.github.com>
2023-09-20 14:13:10 +0300

    Add files via upload

---
+++ C++_Leksiya5_DZ.cpp
@@ -0,0 +1,36 @@
+// #include <iostream>
+
+// int main() {
+//     int base = 2;
+//     int exponent = 10;
+
+//     int result = 1;
+//     int i = 0;
+
+//     while (i < exponent) {
+//         result *= base;
+//         i++;
+//     }
+
+//     std::cout << "2 в степени 10 = " << result;
+
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     int n = 10; 
+//     int factorial = 1; 
+
+//     int i = 1;
+//     while (i <= n) {
+//         factorial *= i;
+//         i++;
+//     }
+
+//     cout << "Факториал числа " << n << " равен: " << factorial;
+
+//     return 0;
+// }
