5dc30309e3e496e62397babefaeaa4a3891726de
krayleft <70761416+krayleft@users.noreply.github.com>
2023-09-20 11:20:47 +0300

    Add files via upload

---
+++ C++_Leksiya3.cpp
@@ -0,0 +1,65 @@
+//1 номер
+
+// #include <iostream>
+
+// int main() {
+//     int x;
+//     std::cout << "Введите число: ";
+//     std::cin >> x;
+
+//     bool powerOf2 = ((x & (x - 1)) == 0);
+
+//     std::cout << "Результат: " << std::boolalpha << powerOf2;
+
+//     return 0;
+// }
+
+//2 номер
+
+// #include <iostream>
+
+// int main() {
+//     int x;
+//     std::cout << "Введите число: ";
+//     std::cin >> x;
+
+//     bool isEven = ((x & 1) == 0);
+
+//     std::cout << "Результат: " << std::boolalpha << isEven;
+
+//     return 0;
+// }
+
+//3 номер
+
+// #include <iostream>
+// #include <cmath>
+
+// int main() {
+//     const double Pi = 3.14159;
+//     double r;
+
+//     std::cout << "Введите радиус окружности: ";
+//     std::cin >> r;
+
+//     double C = 2 * Pi * r;
+
+//     std::cout << "Длина окружности: " << C;
+
+//     return 0;
+// }
+
+//номер 4
+
+// #include <iostream>
+
+// #define cubeof(X) ((X)*(X)*(X))
+
+// int main() {
+//     int number = 10;
+//     int cube = cubeof(number);
+
+//     std::cout << "Куб числа " << number << " равен " << cube;
+
+//     return 0;
+// }
