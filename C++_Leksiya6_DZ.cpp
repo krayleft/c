671cc5cb9c236d0de8aff47fb71fb9446d5c9885
krayleft <70761416+krayleft@users.noreply.github.com>
2023-09-20 14:23:11 +0300

    Add files via upload

---
+++ C++_Leksiya6_DZ.cpp
@@ -0,0 +1,45 @@
+
+// #include <iostream>
+
+// int main() {
+//     for (int i = 1; i <= 100; i++) {
+//         if (i % 2 == 0) {
+//             std::cout << i << " ";
+//         }
+//     }
+
+//     return 0;
+// }
+
+
+
+// #include <iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//   for(int x = 10; x > 0; x-=2) {
+//     cout << x << endl;
+//   }
+
+//     return 0;
+
+// }
+
+
+// #include <iostream>
+
+// int main() {
+//     int sum = 0;
+//     int i = 1;
+
+//     while (sum <= 1000) {
+//         sum += i;
+//         i++;
+//     }
+
+//     std::cout << "Сумма чисел от 1 до " << i-1 << ": " << sum;
+
+//     return 0;
+// }
