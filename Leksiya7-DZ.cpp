e5a09e934c94f4a86eaab01754db5f40b1cbb925
krayleft <70761416+krayleft@users.noreply.github.com>
2023-10-12 12:35:51 +0300

    Update Leksiya7-DZ.cpp

--- Leksiya7-DZ.cpp
+++ Leksiya7-DZ.cpp
@@ -125,3 +125,109 @@
 
 // // 6 задание 
 
+// #include <iostream>
+
+// int main() {
+//     int arr[] = {5, 4, 3, 4, 9};
+//     int max_position = 0;
+//     int max_value = arr[0];
+//     for (int i = 1; i < sizeof(arr) / sizeof(arr[0]); i++) {
+//         if (arr[i] > max_value) {
+//             max_value = arr[i]; 
+//             max_position = i; 
+//         }
+//     }
+
+//     std::cout << "Позиция максимального элемента: " << max_position << std::endl;
+
+//     return 0;
+// }
+
+// // 7 задание
+
+// #include <iostream>
+// #include <cstring>
+ 
+// int main()
+// {
+    
+//     int size = 5;
+//     int *a = new int[size];
+    
+//     for(int i = 0; i < size; ++i)
+//     {
+//         std::cin >> a[i];
+    
+//     }
+//     for(int i = 0, end = size/2; i < end; ++i)
+//     {
+//     int temp = a[i];
+//     a[i] = a[size-1-i];
+//     a[size-1-i] = temp;
+//     }
+    
+//     for(int i = 0; i < size; ++i)
+//     {
+//         std::cout << a[i];
+//     }
+//     return 0;
+// }
+
+// // 8 задание
+
+// #include <iostream>
+
+// int main() {
+//     char stroka[] = "AaBbCcDdEeFfGgggggg";
+//     int count = 0;  
+
+//     for (int i = 0; i < sizeof(stroka); i++) {
+//         if (isupper(stroka[i])) {
+//             count++; 
+//         }
+//     }
+
+//     std::cout << "Количество заглавных букв: " << count << std::endl;
+
+//     return 0;
+// }
+
+// // 9 задание
+
+// #include <iostream>
+
+// int main() {
+//     char stroka[] = "Aaa1323#Aa";
+//     char target = 'A';  // целевая буква для поиска
+//     int count = 0;  // переменная для хранения количества вхождений
+
+//     for (int i = 0; i < sizeof(stroka); i++) {
+//         if (stroka[i] == target) {
+//             count++;  // увеличиваем счетчик, если символ соответствует целевой букве
+//         }
+//     }
+
+//     std::cout << "Количество вхождений буквы A: " << count << std::endl;
+
+//     return 0;
+// }
+
+// // 10 задание
+
+// #include <iostream>
+// #include <cctype>
+
+// int main() {
+//     char stroka[] = "aaaabbbbccccdddd";
+//     int length = sizeof(stroka) - 1; 
+
+//     for (int i = 0; i < length; i++) {
+//         stroka[i] = std::toupper(stroka[i]);
+//     }
+
+//     std::cout << stroka << std::endl; 
+
+//     return 0;
+// }
+
+
